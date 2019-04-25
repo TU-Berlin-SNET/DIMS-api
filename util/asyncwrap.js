f38cbf9cf6891f2f0466e2546327953bc38cc6e0
Ömer Ilhan <malik@mailbox.tu-berlin.de>
2019-04-25 13:46:55 +0200

    set res.locals.result and call next in wrapper, reduces boilerplate

--- util/asyncwrap.js
+++ util/asyncwrap.js
@@ -5,5 +5,14 @@
 
 module.exports = {
     wrap: fn => (req, res, next) => fn(req, res, next).catch(next),
+    wrapNext: fn => (req, res, next) =>
+        fn(req, res, next)
+            .then(result => {
+                if (result) {
+                    res.locals.result = result;
+                }
+            })
+            .then(next)
+            .catch(next),
     wrapEx: fn => (req, res, next, value) => fn(req, res, next, value).catch(next)
 };
